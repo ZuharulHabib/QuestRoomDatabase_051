b6bc2e4be681b151180a2f2d9860ff8ea6bfc9c6
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-12-16 21:51:14 +0700

    Sebuah properti yang harus diimplementasikan oleh semua objek yang mewakili destinasi. Properti ini menentukan path unik atau identitas dari halaman dalam navigasi

--- app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -1,5 +1,26 @@
 package com.example.pertemuan10.ui.navigation
 
+import android.health.connect.datatypes.ExerciseRoute
 interface AlamatNavigasi {
-    val route: String
+    open val route: String
+}
+
+object DestinasiInsert : AlamatNavigasi{   //object akan menjadi nama halaman/ menjadi pengenal halaman
+    override val route: String = "insert_mhs"
+}
+
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{${NIM}}"
 }
