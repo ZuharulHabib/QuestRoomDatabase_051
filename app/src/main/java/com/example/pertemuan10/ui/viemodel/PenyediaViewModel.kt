51766bbd0c82bcaa1e788b0242c30d26ceb543d2
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-12-16 21:55:17 +0700

    berfungsi untuk menyediakan instance ViewModel dengan dependensi yang sesuai menggunakan factory pattern dalam aplikasi berbasis Jetpack Compose.

--- app/src/main/java/com/example/pertemuan10/ui/viemodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viemodel/PenyediaViewModel.kt
@@ -1,20 +1,40 @@
 package com.example.pertemuan10.ui.viemodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan10.KrsApp
 import com.example.pertemuan10.ui.viewmodel.MahasiswaViewModel
 
+
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
-                krsApp().containerApp.repositoryMhs
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer{
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer{
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs
             )
         }
     }
 }
-fun CreationExtras.krsApp(): KrsApp =
+
+fun CreationExtras.KrsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
