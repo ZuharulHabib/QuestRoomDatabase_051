3b8f93c2344a6274d46b4723e5ada239d867f88e
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-12-16 21:45:48 +0700

    Menyisipkan data Mahasiswa ke dalam database. Mengambil seluruh data mahasiswa dari tabel mahasiswa, diurutkan berdasarkan kolom nama secara ascending (ASC). Mengambil data mahasiswa tertentu berdasarkan nim (nomor induk mahasiswa).

--- app/src/main/java/com/example/pertemuan10/Data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/Data/dao/MahasiswaDao.kt
@@ -1,12 +1,31 @@
 package com.example.pertemuan10.Data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //fungsi get all data
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    //get Mahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    //delete mahasiswa
+    @Delete
+    suspend fun  deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    //update Mahasiswa
+    @Update
+    suspend fun  updateMahasiswa(mahasiswa: Mahasiswa)
 }
